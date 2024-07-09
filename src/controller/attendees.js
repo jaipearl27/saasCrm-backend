3815ec6527aa0ec11fca30b8a35b74fb25279eef
jai <jai@pearlorganisation.com>
2024-07-09 15:30:03 +0530

    updates

--- src/controller/attendees.js
+++ src/controller/attendees.js
@@ -23,21 +23,30 @@ export const addAttendees = async (req, res) => {
 
 export const getAttendees = async (req, res) => {
   try {
+    let pipeline = {}
+    if(req?.body?.csvId) pipeline = {csvId: req?.body?.csvId}
+
     
     const page = req?.params?.page || 1;
     const limit = 25;
     const skip = (page - 1) * limit;
+    let totalPages = 0
+        
+   
+    const totalAttendees = await attendeesModel.countDocuments(pipeline);
+    totalPages = Math.ceil(totalAttendees / limit)
     
-    const result = await attendeesModel.find().skip(skip).limit(limit);
-
+    const result = await attendeesModel.find(pipeline).skip(skip).limit(limit);
+    
     res.status(200).json({
       status: true,
       message: "Attendees data found successfully",
       page,
+      totalPages,
       result,
     });
   } catch (error) {
-    res.status(500).json({ status: false, message: error.message });
+    res.status(500).json({ status: false, message: error });
   }
 };
 
@@ -46,6 +55,24 @@ export const getCsvData = async (req, res) => {
     const page = req?.params?.page || 1;
     const limit = 8;
     const skip = (page - 1) * limit;
+
+
+    const countPipeline = [
+      {
+        $group: {
+          _id: "$csvId",
+        },
+      },
+      {
+        $count: "totalUniqueCsvIds"
+      }
+    ];
+
+    const countResult = await attendeesModel.aggregate(countPipeline);
+    const totalUniqueCsvIds = countResult.length > 0 ? countResult[0].totalUniqueCsvIds : 0;
+    const totalPages = Math.ceil(totalUniqueCsvIds / limit);
+
+
     const pipeline = [
       {
         $group: {
@@ -65,7 +92,7 @@ export const getCsvData = async (req, res) => {
 
     const result = await attendeesModel.aggregate(pipeline);
 
-    res.status(200).json({ status: true, page, data: result });
+    res.status(200).json({ status: true, page, totalPages, data: result });
   } catch (error) {
     console.log(error);
     res.status(500).json({ status: false, message: error });
