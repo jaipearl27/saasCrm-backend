0e560477f3c30d20b540bfdb86c51476e83c40fa
jai <jai@pearlorganisation.com>
2024-07-11 17:33:48 +0530

    updates

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import { mongoConnect } from './src/config/db.js'
-import attendeesRouter from './src/routes/attendees.js';
-import cors from 'cors' 
+import dotenv from "dotenv";
+import express from "express";
+import { mongoConnect } from "./src/config/db.js";
+import attendeesRouter from "./src/routes/attendees.js";
+import cors from "cors";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8000
-const app = express()
+const PORT = process.env.PORT || 8000;
+const app = express();
 
 app.use(express.json());
-app.use(cors({
-        origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5010", "https://saas-crm-pi.vercel.app","https://saas-crm-pi.vercel.app/"],
-        methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
-        exposedHeaders: ["*", "Authorization"],
-      }
-))
-app.use('/api/v1/attendee', attendeesRouter)
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5010",
+      "https://saas-crm-pi.vercel.app",
+      "https://saas-crm-pi.vercel.app/",
+    ],
+    credentials: true,
+    methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
+    exposedHeaders: ["*", "Authorization"],
+  })
+);
+app.use("/api/v1/attendee", attendeesRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server Listening on port ${PORT}`)
-    mongoConnect();
-})
+  console.log(`Server Listening on port ${PORT}`);
+  mongoConnect();
+});
--- src/routes/attendees.js
+++ src/routes/attendees.js
@@ -2,7 +2,8 @@ import express from "express";
 import { addAttendees, deleteCsvData, getAttendees, getCsvData } from "../controller/attendees/attendees.js";
 
 const attendeesRouter = express.Router();
-attendeesRouter.route("/:page?").get(getAttendees).post(addAttendees);
+attendeesRouter.route("/").post(addAttendees);
+attendeesRouter.route("/:page?").get(getAttendees).post(getAttendees);
 attendeesRouter.route("/csvData/:page?").get(getCsvData)
 attendeesRouter.route("/:csvId").delete(deleteCsvData)
 
