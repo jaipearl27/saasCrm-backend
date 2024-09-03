f192889ebc7d407c5c7c572535680b37f7506218
Avnish Negi <avnish@pearlorganisation.com>
2024-09-03 17:57:53 +0530

    avdvs

---
+++ src/controller/plans.js
@@ -0,0 +1,19 @@
+import { planModel } from "../models/plans.js";
+import { asyncHandler } from "../utils/errorHandler/asyncHandler.js";
+
+export const addPlan = asyncHandler(async (req, res) => {
+  const { planName } = req?.body;
+  const isPlanExist = await planModel.findOne({ planName });
+  if (isPlanExist) {
+    res.status(500).send("Plan Already exists");
+    return;
+  }
+  await planModel.create(req?.body);
+
+  res.status(200).send("Plan Created Successfully");
+});
+
+export const getPlans = asyncHandler(async (req, res) => {
+  const plans = await planModel.find();
+  res.status(200).json({ status: true, plans });
+});
